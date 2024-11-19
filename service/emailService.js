cda736d13add093507a0d8632166c03d6ac27f2c
adb-santosh <santosh.atithidevobhava@gmail.com>
2024-11-19 11:02:39 +0530

    email service is now dynamically accept conent

--- service/emailService.js
+++ service/emailService.js
@@ -3,10 +3,12 @@ import jwt from "jsonwebtoken";
 import CustomError from "../utils/CustomError.js";
 
 
-const sendVerificationEmail = async (email,userId) => {
+const sendVerificationEmail = async (email,userId,content) => {
 
     // Generate a verification token and send it to the user's email using nodemailer.
 try {
+
+  console.log(email,userId+"from email")
     const verificationToken = jwt.sign({ email, userId }, process.env.JWT_SECRET, { expiresIn: process.env.EMAIL_EXP });
 
     // smtp server creation for this 
@@ -21,18 +23,15 @@ try {
 
     // Construct the email content with the verification link
     
+    const emailContent = content(verificationToken);
+
     const mailOptions = {
-        from: process.env.EMAIL_ADDRESS,
-        to: email,
-        subject: "Verify Your Email",
-        html: `
-          <p>Hello,</p>
-          <p>Please verify your email using the link below:</p>
-          <a href="${process.env.BASE_URL}/api/v1/users//verify-email?token=${verificationToken}">Verify Email</a>
-          <p>This link will expire in 10 minutes.</p>
-          <p>Regards,<br>Your App Team</p>
-        `,
-      };
+      from: process.env.EMAIL_ADDRESS,
+      to: email,
+      subject: emailContent.subject,
+      text: emailContent.text,
+      html: emailContent.html,
+    };
     
     // send the mail to user
       await transporter.sendMail(mailOptions);
