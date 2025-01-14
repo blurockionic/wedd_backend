d05dbe773f83441d5c0b935f2d9c793371b6d6ff
adb-santosh <santosh.atithidevobhava@gmail.com>
2025-01-14 14:45:21 +0530

    update controller updated

--- controllers/userController/update.js
+++ controllers/userController/update.js
@@ -1,5 +1,4 @@
-import CustomError from "../../utils/CustomError.js";
-import z from "zod";
+
 import { updateUserSchema } from "../../validation schema/user.schema.js";
 import { PrismaClient } from "../../prisma/generated/postgres/index.js";
 import upload from "../../middleware/multer.middleware.js";
@@ -41,21 +40,11 @@ const updateUser = async (req, res, next) => {
     res.status(200).json({
       message: "User updated successfully.",
       user: userResponse,
+      success:true
     });
   } catch (error) {
-    console.error(`Error Type: ${error.constructor.name}, Message: ${error.message}, Stack: ${error.stack}`);
-
-    if (error.code === "P2025") {
-      // Handle case where user does not exist
-      return next(new CustomError("User not found.", 404));
-    }
-
-    if (error instanceof z.ZodError) {
-      // Handle validation errors
-      return res.status(400).json({ errors: error.errors });
-    }
-
-    next(new CustomError("An error occurred during user update.", 500));
+    
+    next();
   }
 };
 
