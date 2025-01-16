3325e258b27bf37aebcfe0c3e78ef29b334c5060
adb-santosh <santosh.atithidevobhava@gmail.com>
2025-01-16 12:33:10 +0530

    lead implemented

---
+++ controllers/serviceController/lead.service.controller.js
@@ -0,0 +1,37 @@
+import { PrismaClient } from "../../prisma/generated/mongo/index.js";
+
+const prisma = new PrismaClient();
+
+export const makeLead = async (req, res, next) => {
+  try {
+    const serviceId = req.params.id;
+    const userId = req.user?.id;
+    const userRole = req.user?.role?.toLowerCase();
+
+    if (!serviceId || !userId) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required parameters: serviceId or userId.",
+      });
+    }
+
+    const updatedView = await prisma.Views.update({
+      where: {
+        serviceId_userId: {
+          serviceId: serviceId,
+          userId: userId,
+        },
+      },
+      data: {
+        lead: true,
+      },
+    });
+
+    // Respond with the service details
+    res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
