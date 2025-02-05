342a5dd5ec70bc7dfecca3fbed593910fc7c0c8f
adb-santosh <santosh.atithidevobhava@gmail.com>
2025-02-05 13:02:39 +0530

    payment adn serviec bug fixed

--- routes/subscribe.route.js
+++ routes/subscribe.route.js
@@ -1,18 +1,17 @@
 import express from "express";
-import { createOrder, verifyPayment } from "../controllers/payment/payment.js";
+import { createOrder, getPaymentHistory, getSubscription, verifyPayment } from "../controllers/payment/payment.js";
 import jwtAuthentication from "../middleware/auth.middleware.js";
-import roleMiddleware from "../middleware/role.middleware.js";
+
 
 
 
 const subscribeRouter = express.Router();
 
-subscribeRouter.use(jwtAuthentication,);
-// POST /create-plan'
+subscribeRouter.use(jwtAuthentication);
+
 subscribeRouter.post("/create-order/:planId",createOrder);
 subscribeRouter.post("/verify-payment",verifyPayment)
-
-// // PUT /edit-plan/:id
-// subscribeRouter.put("/edit-plan/:id", editPlan);
+subscribeRouter.get("/payment-history",getPaymentHistory)
+subscribeRouter.get("/subscription",getSubscription)
 
 export default subscribeRouter;
