aba7bad8b767ebc4e0256e0da47d660c48e9bff0
RamenDevourer <priyanshuburde2004@gmail.com>
2025-02-19 01:53:36 +0530

    services general analytics
    added controllers for
    - get total service count
    - get total views for services
    - get total leads for services
    - get total views group by service type
    - get total leads group by service type

--- controllers/adminController/generalAnalytics.controller.js
+++ controllers/adminController/generalAnalytics.controller.js
@@ -12,12 +12,71 @@ const getTotalVendors = async () => {
     return await prismaMongo.vendor.count();
 };
 
+const getTotalServices = async () => {
+    return await prismaMongo.service.count();
+};
+
+const getServicesData = async () => {
+    return await prismaMongo.views.aggregate({
+        _sum: {
+          viewCount: true,
+        },
+        _count: {
+          lead: true,
+        }
+    })
+};
+
+const getMostViewedServicesData = async () => {
+    return  await prismaMongo.views.aggregateRaw({
+        pipeline: [
+          {
+            $lookup: {
+              from: "Service",
+              localField: "serviceId",
+              foreignField: "_id",
+              as: "serviceDetails",
+            },
+          },
+          { $unwind: "$serviceDetails" }, // Flatten lookup array
+          {
+            $group: {
+              _id: "$serviceDetails.service_type",
+              totalViewCount: { $sum: "$viewCount" }, // Sum of all view counts
+              totalLeads: {
+                $sum: {
+                  $cond: {
+                    if: { $eq: ["$lead", true] },
+                    then: 1,
+                    else: 0,
+                  },
+                },
+              },
+            },
+          },
+          {
+            $project: {
+              service_type: "$_id",
+              _id: 0,
+              totalViewCount: 1,
+              totalLeads: 1,
+            },
+          },
+        ],
+      });
+      
+      
+};
+
 const generalAnalytics = async (req, res) => {
     try {
         const response = {};
 
         response.totalUsers = await getTotalUsers();
         response.totalVendors = await getTotalVendors();
+        response.totalServices = await getTotalServices();
+        response.getServicesData = await getServicesData();
+        response.getMostViewedServicesData = await getMostViewedServicesData();
         // response.totalRevenue = await getTotalRevenue();
     
         return res.status(200).json({ success: true, data: response });
