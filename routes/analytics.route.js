5f9c2e12c03d013781c2bc65948d1c596ff4ebf3
Blurock_Santosh <santosh.atithidevobhava@gmail.com>
2025-03-17 15:05:57 +0530

    Update analytics.route.js

--- routes/analytics.route.js
+++ routes/analytics.route.js
@@ -3,17 +3,16 @@ import jwtAuthentication from "../middleware/auth.middleware.js";
 import roleMiddleware from "../middleware/role.middleware.js";
 import getVendorDashboardData from "../controllers/anlytics controller/view.controller.js";
 import { leaddetails } from "../controllers/serviceController/lead.service.controller.js";
-
-
+import { getHeroSectionAnalytics } from "../controllers/anlytics controller/getHeroSectionAnalytics.controller.js";
 
 const AnalyticsRouter = express.Router();
 
-// Routes for cart operations
-AnalyticsRouter.use(jwtAuthentication, roleMiddleware(["VENDOR","ADMIN"]));
+AnalyticsRouter.get("/heroSectionAnalytics", getHeroSectionAnalytics);
+
+AnalyticsRouter.use(jwtAuthentication, roleMiddleware(["VENDOR", "ADMIN"]));
 
 //clear the cart
 AnalyticsRouter.get("/getViewData", getVendorDashboardData);
-AnalyticsRouter.route("/getlead").get(leaddetails)
-
+AnalyticsRouter.route("/getlead").get(leaddetails);
 
 export default AnalyticsRouter;
