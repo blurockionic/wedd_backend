38f6d3351540c435e0ad4f707b8ff53e061ebe23
adb-santosh <santosh.atithidevobhava@gmail.com>
2025-04-07 09:46:05 +0530

    feature(payment ): for templates

--- routes/templatePayment.route.js
+++ routes/templatePayment.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import jwtAuthentication from "../middleware/auth.middleware.js";
-import { createPayment, verifyPayment } from "../controllers/templatePayment.controller.js";
+import { createPayment, templatePaymentStatus, verifyPayment } from "../controllers/templatePayment.controller.js";
 
 
 const templatePaymentRouter = express.Router();
@@ -9,5 +9,6 @@ templatePaymentRouter.use(jwtAuthentication);
 
 templatePaymentRouter.post("/create-order/:cardId",createPayment);
 templatePaymentRouter.post("/verify-payment", verifyPayment);
+templatePaymentRouter.get("/payment-status/:templateId",templatePaymentStatus );
 
 export default templatePaymentRouter;
